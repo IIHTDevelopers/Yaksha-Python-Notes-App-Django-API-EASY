e822391cbd154196ef1495e8ece880ffefb38948
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-05-06 13:18:45 +0530

    Update test_exceptional.py

--- notesapp/test/test_exceptional.py
+++ notesapp/test/test_exceptional.py
@@ -1,8 +1,7 @@
 from rest_framework.test import APITestCase
-from notesapp.models import NotesModel
 from notesapp.test.TestUtils import TestUtils
-class NotesAppAPIExceptionalTest(APITestCase):
+class NotesAppAPIBoundaryTest(APITestCase):
     def test_exceptional(self):
         test_obj = TestUtils()
-        test_obj.yakshaAssert("Testexceptional",True,"exceptional")
+        test_obj.yakshaAssert("TestExceptional",True,"boundary")
         print("TestExceptional = Passed")
